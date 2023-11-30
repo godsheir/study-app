28960f2446cbddbab84b1092bc1407f3eb82478f
godsheir <angelgodsheir@gmail.com>
2023-11-30 12:27:22 -0300

    Updates

---
+++ assets/study-app-data-f394d4ae.js
@@ -0,0 +1,121 @@
+const e=[["Psicología Diferencial",`Es la disciplina que pretende
+describir, explicar y predecir
+la variabilidad psicológica
+utilizando una aproximación
+científica a este fenómeno`,"#ff8080","Psicología Diferencial"],["Personalidad",`Patrón único de pensamientos, sentimientos y conductas de un individuo que 
+persiste a través del tiempo y de las situaciones`,"#ffff80",""],["Principio de placer",`De acuerdo con Freud, la forma en que el ello busca la gratificación 
+inmediata de un instinto.`,"#8080ff","Psicología Diferencial"],["Temperamento",`Patrones característicos de reacciones emocionales y autorregulación 
+emocional.`,"#00ff80","Psicología Diferencial"],["Carácter",`es el sello que nos identifica y diferencia de
+nuestros semejantes, producto del aprendizaje social`,"#80ffff","Psicología Diferencial"],["Personalidad para Freud",`Teoría de la personalidad que afirma que la conducta es el resultado de fuerzas psicológicas 
+que interactúan dentro del individuo, a menudo fuera de la conciencia.`,"#0080ff","Psicología Diferencial"],["Inconsciente",`Todas las ideas, pensamiento y sentimiento de los cuales normalmente no podemos 
+tomar conciencia.`,"#ff80c0","Psicología Diferencial"],["Psicoanálisis",`Teoría de la personalidad desarrollada por Freud así como la forma de terapia que él 
+propuso.`,"#ff80ff","Psicología Diferencial"],["Ello (id)",`En la teoría de la personalidad de Freud, el conjunto de impulsos y deseos inconscientes 
+que continuamente buscan expresión.`,"#ff8080","Psicología Diferencial"],["Mecanismos de defensa","Técnicas de autoengaño para reducir la ansiedad y la culpa, como la negación, represión, proyección, identificación, regresión, intelectualización, formación reactiva, desplazamiento y sublimación","#ffff80","Psicología Diferencial"],["Inconsciente colectivo",` en la teoría de la personalidad de Jung, el nivel de inconsciente que es 
+heredado y común a todos los miembros de la especie.`,"#80ff80","Psicología Diferencial"],["Teoría humanista de la personalidad",`Énfasis en una motivación positiva y progresiva hacia niveles superiores de 
+funcionamiento; la existencia humana no se limita manejar conflicto oculto. 
+Proceso de apertura al mundo que permite experimentar la dicha de vivir.`,"#00ff40","Psicología Diferencial"],["Cinco grandes factores",`Extraversión, afabilidad, escrupulosidad/confiabilidad, estabilidad emocional, 
+cultura/intelecto/apertura.`,"#00ffff","Psicología Diferencial"],["Introversión-extroversión","Grado en que una persona está orientada al interior o al exterior","#0080c0","Psicología Diferencial"],["Trastorno mental",`Es un síndrome o un patrón comportamental o
+psicológico de significación clínica, que aparece
+asociado a un malestar, a una discapacidad o a un
+riesgo significativamente aumentado de morir o
+sufrir dolor, discapacidad o pérdida de libertad.`,"#8080c0","Psicología Diferencial"],["La genética",`Es la rama de la biología que se encarga del estudio
+de aquello es transmitido en sucesivas generaciones
+a través de
+los genes. El concepto también hace referencia
+a lo que se vincula
+con el comienzo
+, el inicio
+o la raíz de algo`,"#ff00ff","Psicología Diferencial"],["Ambiente ",`Indica las condiciones o circunstancias de un lugar, por 
+lo que se puede hablar de un “buen ambiente”, “ambiente 
+propicio” o “mal ambiente”. Un ambiente hostil se refiere al caso 
+de un entorno social, psicológico o físico que violenta contra el 
+bienestar de un ser vivo, volviéndolo vulnerable`,"#af5f5f","Psicología Diferencial"],["Fenotipo","Características observables por medio de las cuales se expresa su estructura genética subyacente.","#ff8040","Psicología Diferencial"],["La plasticidad neuronal",`Hace referencia
+a una propiedad de nuestro sistema nervioso que 
+le permite adaptarse de forma constante a las 
+experiencias vitales que se van sucediendo.`,"#00ff00",""],["Thorndike",`revaluó las hipótesis de las diferencias de sexos en dos estudios
+llegando a la conclusión de que
+mujeres y hombres eran muy
+similares en general, y que el sexo
+sólo podía explicar un porcentaje
+muy pequeño de las diferencias
+mentales en las capacidades
+analizadas`,"#008080",""],["Fechner",`La psicofísica aparece por primera vez con
+esta denominación de su mano, médico y físico que pretendía probar, de forma experimental, la relación
+entre el cuerpo y el alma.`,"#8080ff",""],["Paul Broca",`Estudio las
+lesiones en áreas concretas
+que manifestaban
+determinadas alteraciones
+conductuales, permitió estudiar
+el cerebro de forma científica.`,"#800040",""],["La teoria de la inteligencia de Cattell",`1) las capacidades, que se refieren a las
+potencialidades de nuestro cerebro;
+
+2) los poderes provinciales, que consisten en
+organizaciones locales para las distintas
+modalidades sensoriales y motoras;
+
+3) las agencias, que serían aquellas habilidades
+necesarias para la ejecución de distintas tareas
+propias de un grupo cultural`,"#ff0080",""],["La inteligencia fluida",`Es «la capacidad de adaptarse a problemas o situaciones nuevas, sin necesidad de
+experiencias anteriores de aprendizaje o de ayudas, al menos de forma decisiva»
+`,"#800000",""],["La inteligencia cristalizada",`Recoge las «capacidades cognitivas en las que podrían haber cristalizado los efectos
+acumulados del aprendizaje anterior»
+`,"#408080",""],["La comprensión verbal",`Es la capacidad para entender el propio lenguaje. Algunas de las pruebas
+empleadas son la comprensión de textos, las analogías verbales, pruebas de
+ortografía, ordenación correcta de palabras y frases, o lectura comprensiva`,"#808000",""],["El factor numérico.",`Es la capacidad para resolver tareas numéricas sencillas, por ejemplo, sumas, restas
+o multiplicaciones`,"#808040",""],["Superdotado",`Es aquella persona cuyas capacidades son
+superiores a las normales o a las esperadas
+para su edad y condición, en una o en
+varias áreas de la conducta humana.`,"#800080",""],["Inteligencia para Howard  Gardner",`Capacidad de 
+resolver problemas 
+o de crear 
+productos que sean 
+valiosos en una o 
+más culturas`,"#004080",""],["Super yo (super ego)",`De acuerdo con Freud, los estándares sociales y de los padres que el 
+individuo ha interiorizado; la conciencia y el yo ideal.`,"#8080ff",""],["Libido","La energía generada por el instinto sexual.","#004080",""],["Fijación",`De acuerdo con Freud, una detención parcial o completa en cierto punto del desarrollo psicosexual 
+del individuo.`,"#ff8040",""],["Inconsciente personal",`En la teoría de la personalidad de Jung, uno de los dos niveles del 
+inconsciente; contiene los pensamientos reprimidos del individuo, sus experiencias olvidadas e 
+ideas no desarrolladas.`,"#0080ff",""],["Persona",`De acuerdo con Jung, nuestro yo público, la máscara que nos ponemos para 
+representarnos ante los demás.`,"#008080",""],["Carl Rodgers",`Afirma la bondad fundamental de la gente y su esfuerzo por alcanzar niveles 
+superiores de funcionamiento.`,"#ffffff",""],["Características de la personalidad madura",`• Objetividad
+• Autonomía
+• Capacidad de amar
+• Sentido de responsabilidad
+• Flexibilidad
+• Reflexión
+• Sentido del humor
+• Autocontrol
+• Libertad`,"#8080c0",""],["Teoría del aprendizaje",`La persona es un agente activo capaz de
+conocerse a sí mismo (valorar los refuerzos), regular su conducta (anticipar
+consecuencias) y afrontar los problemas de la vida cotidiana.`,"#800040",""],["Neurosis",`Trastornos mentales que distorsiona el pensamiento racional y el 
+funcionamiento a nivel social, familiar y laboral adecuado de las personas.`,"#004080",""],["Psicosis",`Estado mental descrito como una escisión o pérdida de contacto con la 
+realidad`,"#0080c0",""],["Sociopatía",`Patología que deriva en que las personas que la padecen pierden la noción de 
+la importancia de las normas sociales, como son las leyes y los derechos 
+individuales.
+`,"#ffffff",""],["La psicopatología",`Es el estudio de las causas y
+naturaleza de las enfermedades mentales. Puede
+desarrollarse según distintos enfoques o modelos, como el
+biomédico, el psicodinámico, el socio-biológico y el
+conductual, entre otros.
+`,"#800080",""],["Precoces",`Los que tienen un 
+desarrollo temprano inusual 
+para su edad.`,"#8080ff",""],["Prodigio",`Son los que realizan una 
+actividad fuera de lo común 
+para su edad y condición. 
+Desarrollan productos que 
+llaman la atención en un 
+campo específico, memoria, 
+lenguaje, ritmo, etcétera.`,"#80ff80",""],["Genios",`Bajo este 
+término se encontrarían sujetos 
+con una gran capacidad
+intelectual y de producción`,"#004080",""],["Teorías factoriales de la inteligencia",`Las que identifican sus propiedades o
+componentes a través de un procedimiento matemático o
+estadístico`,"#8080ff",""],["Las teorías jerárquicas de la inteligencia",`plantean que está estructurada en forma de
+pirámide, situándose en la base las capacidades
+más específicas y en posiciones superiores las
+generales`,"#008040",""],["La inducción o razonamiento general",`Es la capacidad para extraer una regla general de una serie de números o símbolos
+que están ordenados siguiendo un criterio más o menos complejo, pasando de lo
+específico a lo general.`,"#408080",""],["THURSTONE",`Capacidad para inhibir las respuestas instintivas.
+
+Considera que la persona inteligente es aquella que, antes de que se produzca dicha
+respuesta, valora si es o no la más óptima para su bienestar.
+`,"#8080ff",""]];export{e as default};
